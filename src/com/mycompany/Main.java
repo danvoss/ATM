e0d93ba30ff3f7af17059959d274c7eb91069c56
Dan <danielandrewvoss@gmail.com>
2016-05-20 12:23:47 -0400

    Add and remove accounts

--- src/com/mycompany/Main.java
+++ src/com/mycompany/Main.java
@@ -14,63 +14,60 @@ public class Main {
 
         //start loop here somehow?
 
-        System.out.println("Hello. What is your name?");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
+        while (true) {
+            System.out.println("Hello. What is your name?");
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
 
-        if (name.isEmpty()) {
-            throw new Exception("You must enter your name.");
-        }
-        else {
-            System.out.println("Hi, " + name + ".");
-        }
-
-        if (!(customerBalances.containsKey(name))) {
-            System.out.println("Would you like to create an account? [Y/N]");
-            String accountAnswer = scanner.nextLine();
-            if (accountAnswer.equalsIgnoreCase("N")) {
-                System.out.println("Fine, then. Goodbye.");
+            if (name.isEmpty()) {
+                throw new Exception("You must enter your name.");
+            } else {
+                System.out.println("Hi, " + name + ".");
             }
-            else {
-                System.out.println("How much money do you have?");
-                String balanceAnswer = scanner.nextLine();
-                double balanceDouble = Double.valueOf(balanceAnswer);
-                customerBalances.put(name, balanceDouble);
-            }
-        }
-        else {
-            System.out.println("What would you like to do? Enter a number.");
-            System.out.println("1. Check balance 2. Withdraw funds 3. Remove account 4. Cancel");
-            String number = scanner.nextLine();
 
-            while (!(number.equals("4"))) {
-                switch (number) {
-                    case ("1"):
-                        Double currentBalance = customerBalances.get(name);
-                        System.out.println(currentBalance);
-                        break;
-                    case ("2"):
-                        System.out.println("How much would you like to withdraw?");
-                        String amount = scanner.nextLine();
-                        double amountDouble = Double.valueOf(amount);
-                        if (amountDouble <= customerBalances.get(name)) {
-                            System.out.println("Please take your cash.");
-                        }
-                        else {
-                            throw new Exception ("That amount exceeds your balance.");
-                        }
-                        break;
-                    case ("3"):
-                        customerBalances.remove(name);
-                        break;
-                    default:
-                        System.out.println("Please enter 1, 2, 3, or 4.");
-                        break;
+            if (!(customerBalances.containsKey(name))) {
+                System.out.println("Would you like to create an account? [Y/N]");
+                String accountAnswer = scanner.nextLine();
+                if (accountAnswer.equalsIgnoreCase("N")) {
+                    System.out.println("Fine, then. Goodbye.");
+                } else {
+                    System.out.println("How much money do you have?");
+                    String balanceAnswer = scanner.nextLine();
+                    double balanceDouble = Double.valueOf(balanceAnswer);
+                    customerBalances.put(name, balanceDouble);
                 }
-                number = scanner.nextLine();
-            }
-            System.out.println("Thank you. Goodbye.");
+            } else {
+                System.out.println("What would you like to do? Enter a number.");
+                System.out.println("1. Check balance 2. Withdraw funds 3. Remove account 4. Cancel");
+                String number = scanner.nextLine();
 
+                while (!(number.equals("4"))) {
+                    switch (number) {
+                        case ("1"):
+                            Double currentBalance = customerBalances.get(name);
+                            System.out.println(currentBalance);
+                            break;
+                        case ("2"):
+                            System.out.println("How much would you like to withdraw?");
+                            String amount = scanner.nextLine();
+                            double amountDouble = Double.valueOf(amount);
+                            if (amountDouble <= customerBalances.get(name)) {
+                                System.out.println("Please take your cash.");
+                            } else {
+                                throw new Exception("That amount exceeds your balance.");
+                            }
+                            break;
+                        case ("3"):
+                            customerBalances.remove(name);
+                            break;
+                        default:
+                            System.out.println("Please enter 1, 2, 3, or 4.");
+                            break;
+                    }
+                    number = scanner.nextLine();
+                }
+                System.out.println("Thank you. Goodbye.");
+            }
         }
     }
 }
